d0126126add9410c6e6c6046916b44460491e920
Lucas Alves Dos Santos <lucas201273@gmail.com>
2021-01-04 16:12:45 -0300

    aula04

---
+++ mod1-JsBasico/aula04/index.js
@@ -0,0 +1,18 @@
+// Aula 04: Variáveis com let
+let nome;
+console.log(nome) //  undefined diz que a variável não tem valor inicializado
+nome = 'João'
+console.log(nome,' nasceu em 1984');
+console.log('Em 2000', nome , 'conheceu Maria');
+console.log(nome ,'casou-se com Maria em 2012');
+console.log(' Maria teve 1 filho com ', nome,' em 2015');
+console.log('O filho de ' , nome ,' se chama Eduardo ');
+
+nome = 'Lucas';
+
+// Regras para variáveis.
+// Adiconar nomes descritos para variáveis.
+// Não podemos criar variávies com palavras reservadas.
+// Não podem conter espaços ou traços
+// Utilizamos camelcase.
+// Case-sentive -> letras maisculas e letras minusculás fazem diferenças.
