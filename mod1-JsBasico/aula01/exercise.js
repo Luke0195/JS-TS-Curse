e6a3b259da56c65d356ba48d70a25ee6f9c9970f
Lucas Alves Dos Santos <lucas201273@gmail.com>
2021-01-04 15:06:22 -0300

    aula01

---
+++ mod1-JsBasico/aula01/exercise.js
@@ -0,0 +1,1 @@
+console.log('Meu nome é Lucas.Estou aprendendo JavaScript ás', 10, 'da manhã');
---
+++ mod1-JsBasico/aula01/index.js
@@ -0,0 +1,10 @@
+// Aula 01 -> Console.log
+
+console.log(" Lucas 'Alves' ");
+console.log('Lucas  "Alves" ');
+console.log(`"Lucas" 'Alves'`);
+
+console.log(3);
+console.log(1,2,3,4,5,6);
+console.log('O linux é nota', 10);
+// O utilizamos console.log para debugar o a aplicação
