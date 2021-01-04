6eb81babc3fabd8a70bb3a39066712f176c89e7a
Lucas Alves Dos Santos <lucas201273@gmail.com>
2021-01-04 15:21:07 -0300

    aula02

---
+++ mod1-JsBasico/aula02/index.js
@@ -0,0 +1,14 @@
+// Aula 02: Comentários -> Comentário de linha 
+console.log('Linha 1');
+console.log('Linha 2');
+console.log('Linha 3');
+console.log('Linha 4');
+console.log('Linha 5');
+// Vai ser ignorado
+
+/** Comentário de bloco
+ * Comentário linha 1
+ * Comentário linha 2
+ * Comentário linha 3
+ * Comentário linha 4
+ */
