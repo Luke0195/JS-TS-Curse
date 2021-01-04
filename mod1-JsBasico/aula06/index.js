1e407bbe1ec43e744ea3d02304a84e86a671e3d6
Lucas Alves Dos Santos <lucas201273@gmail.com>
2021-01-04 16:50:59 -0300

    aula06

---
+++ mod1-JsBasico/aula06/index.js
@@ -0,0 +1,11 @@
+// Aula 06: Let vs Var - Primeira diferença
+// ECMAScript 2015(ES6)
+var nome = 'Lucas';
+var nome = 'Mathues';
+console.log(nome);
+
+texto = 'Hello Stranger';
+
+console.log(nome, texto);
+
+// Primeira diferença entra var e let, não podemos redeclarar a variável let utilizando a palavra reservada.
