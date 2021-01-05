b05f81fddd588d3290116b7325d51e2d66bfc05e
Lucas Alves Dos Santos <lucas201273@gmail.com>
2021-01-05 18:10:47 -0300

    aula09

---
+++ mod1-JsBasico/aula09/index.js
@@ -0,0 +1,21 @@
+// Exercicio de 
+
+let varA = 'A';
+let varB = 'B';
+let varC = 'C';
+
+let varD =  varA;
+varA = varB;
+varB = varC;
+varC = varD;
+
+console.log(varA, varB, varC);
+
+
+// B
+// C 
+// A
+
+const [varA, varB, varC] = ['B', 'C', 'A'];
+
+console.log(varA, varB, varC);
