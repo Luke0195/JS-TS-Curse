7ffe1b569a8c8910adedc760eb7c96972395cf24
Lucas Alves Dos Santos <lucas201273@gmail.com>
2021-01-05 17:13:13 -0300

    aula07

---
+++ mod1-JsBasico/aula07/index.js
@@ -0,0 +1,25 @@
+// Aula 07: Operadores aritmético, de atribuição e incremento
+
+// Operadores Aritmeticos
+/**
+ *  + Soma ou concatenação
+ *  - Subtração
+ *  * multiplicação
+ *  / Divisão
+ *  % Resto da divisão
+ *  ** contenciação
+ */
+
+ const num1 = 5;
+ const num2 = 10;
+ console.log(num1 % num2);
+
+ /**
+  *  ++ incremento
+  *  -- decremento 
+  */
+ 
+  const passo = 50;
+  let contador = 0;
+  contador += passo;
+  console.log(contador); // atribução
