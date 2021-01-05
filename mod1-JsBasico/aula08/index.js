b1220fc6c0384c9e504334e318426dd45b71abd9
Lucas Alves Dos Santos <lucas201273@gmail.com>
2021-01-05 17:49:08 -0300

    aula08

---
+++ mod1-JsBasico/aula08/index.js
@@ -0,0 +1,5 @@
+const num1 = prompt('Digite primeiro número');
+const num2 = prompt('Digite segundo número');
+const resultado  = Number(num1) + Number(num2);
+
+alert(`O resultado  é ${resultado}`);
