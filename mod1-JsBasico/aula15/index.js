61c0247fe80d4c203c15a0fb38f29f89d641ff63
Lucas Alves Dos Santos <lucas201273@gmail.com>
2021-01-06 19:08:52 -0300

    aula15

--- mod1-JsBasico/aula15/index.js
+++ mod1-JsBasico/aula15/index.js
@@ -1,1 +1,31 @@
-// Aula 15: Objetos (Básico)
+// Aula 15: Objetos(Básicos)
+
+// Objeto Literal;
+const pessoa1 = {
+  nome: 'Lucas',
+  sobrenome: 'Santos',
+  idade: 26,
+  showInfo(){
+    console.log('Informations of this Person');
+    console.log(`First name: ${this.nome}`);
+    console.log(`Last Name: ${this.sobrenome}`);
+    console.log(`Age : ${this.idade}`)
+  },
+
+  incrementAge(){
+    this.idade ++;
+  }
+}
+// função fabrica
+function criarPessoa(nome, sobrenome, idade){ // Função Factory(Fabríca)
+  return{
+    nome,
+    sobrenome,
+    idade
+  }
+}
+
+
+pessoa1.showInfo();
+pessoa1.incrementAge();
+pessoa1.showInfo();
