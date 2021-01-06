f46ea37de62b860538a272b68b8391e821ddacbf
Lucas Alves Dos Santos <lucas201273@gmail.com>
2021-01-06 19:21:08 -0300

    aula16

---
+++ mod1-JsBasico/aula16/index.js
@@ -0,0 +1,8 @@
+// Aula 16: Valores primitivos e valores por referência
+
+// Tipos primitivos -> String, Number, Boolean, Symbol, undefined(bigint, )
+
+
+let nome = 'Lucas';
+nome[0] = 'S';
+console.log(nome);
