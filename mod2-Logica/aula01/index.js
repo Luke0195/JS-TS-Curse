0fb188f03d5bb94e2f5d64fb4cbc33fc2ee408e6
Lucas Alves Dos Santos <lucas201273@gmail.com>
2021-01-07 06:19:38 -0300

    aula01

---
+++ mod2-Logica/aula01/index.js
@@ -0,0 +1,26 @@
+// Aula 01 : Operados de  Comparação
+
+/**
+ *  > -> maior que 
+ *  >=  -> maior ou igual
+ *  =  -> atribuição
+ *  < -> menor
+ *  <= -> menor igual
+ *  == -> igualdade(valor)
+ *  === -> igualdade restrita *valor e tipo)
+ *  != -> diferente
+ *  !== -> diferente estrito(verifica valor e tipo)
+ */
+
+const num1 =10;
+const num2 = '10';
+
+const comp = num1 === num2; // verica o valor e o tipo
+console.log(comp);
+
+
+const numero1= 8;
+const numero2 = '8'
+
+const comp2 = num1 !== num2;
+console.log(comp2)
