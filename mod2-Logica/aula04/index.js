506647690da40305593c982c9a24deac755f1894
Lucas Alves Dos Santos <lucas201273@gmail.com>
2021-01-07 07:37:20 -0300

    aula04

---
+++ mod2-Logica/aula04/index.js
@@ -0,0 +1,35 @@
+// Aula 04: if, else if , else(1).
+
+// Entre 0 - 11 => Bom dia
+// Entre 12 - 17 => Boa Tarde
+// Entre 18 - 23 => Boa Noite 
+/*
+const hora = 'a';
+
+if(hora >= 0 &&  hora <= 11){
+  console.log(' Bom dia');
+}else if( hora >=12 && hora <= 17){
+  console.log(' Boa Tarde');
+}else if( hora >= 18 && hora <= 23){
+  console.log('Boa Noite');
+}else{
+  console.log('Opção invalida')
+}
+const haveMoney = true;
+
+if(!haveMoney){
+  console.log('Bora gastar');
+}else{
+  console.log('Melhor ficar em casa')
+}
+*/
+
+// Aula if, else if, else (2)
+
+const numero = 10;
+
+if (numero >= 0 && numero <= 5){
+  console.log('O numero está no intervalo entro 0 e 5');
+}else{
+  console.log(' O número não está no intervalo');
+}
