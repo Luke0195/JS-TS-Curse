dcc970708a6762360e1bde350349f2fa1538fb3a
Lucas Alves Dos Santos <lucas201273@gmail.com>
2021-01-07 06:41:31 -0300

    aula02

---
+++ mod2-Logica/aula02/index.js
@@ -0,0 +1,18 @@
+// Aula 02:  Operados Lógicos
+// Operadores Lógicos
+// && -> and -> E
+// || -> or -> out
+// ! -> not -> não
+
+console.log(  true && true);
+console.log( true || false);
+
+const userName = 'Lucas';
+const userPassword = '3030'
+
+
+const userIsLogged =  userName === 'Lucas' && userPassword === '3008';
+
+console.log(userIsLogged);
+
+console.log(!true);
