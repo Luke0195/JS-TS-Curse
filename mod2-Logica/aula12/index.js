062ffb90fb4e8df828f84382a2d20b8790a0194b
Lucas Alves Dos Santos <lucas201273@gmail.com>
2021-01-08 10:51:36 -0300

    aula12

---
+++ mod2-Logica/aula12/index.js
@@ -0,0 +1,7 @@
+// Aula 12: For Clássico - Estrutura de repetição
+
+const frutas = ['Maça', 'Morango', 'Péra', 'Uva']
+
+for( let i = 0 ; i < frutas.length ; i++){
+  console.log(`${i} - ${frutas[i]}`);
+}
