f7c37d124e3c9361c16011a7cf3eb491e245fdfe
Lucas Alves Dos Santos <lucas201273@gmail.com>
2021-01-12 20:37:38 -0300

    done modulo4

---
+++ mod4-arrays/aula08/index.js
@@ -0,0 +1,7 @@
+// Aula 08:  ForEach
+
+// O forEach é a forma que utilizamos interar sobre array.
+
+const numeros = [5, 50, 80, 1, 2, 3, 5, 8, 7, 11, 15, 22, 27];
+
+numeros.forEach(valor => console.log(valor));
