85c889f7ef9c06b412d6fbe15d996b04e5cf7708
Lucas Alves Dos Santos <lucas201273@gmail.com>
2021-01-12 20:35:09 -0300

    aula07

---
+++ mod4-arrays/aula07/index.js
@@ -0,0 +1,15 @@
+// Aula 07: Filter + Map + Reducer
+
+// Filtar pares
+// Dobrar  os valores
+// Reduzir (somar tudo)
+
+const numeros = [5, 50, 80, 1, 2, 3, 5, 8, 7, 11, 15, 22, 27];
+
+const filtrarElementos = numeros.filter(valor => valor % 2 === 0)
+.map(valor => valor * 2)
+.reduce(function(acumulador, valor){
+  acumulador += valor;
+  return acumulador
+},0)
+console.log(filtrarElementos);
